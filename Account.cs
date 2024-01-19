1a80fd2ca2a95675f89e2be407126ec0ad394bba
Iqra210042109 <128312944+Iqra210042109@users.noreply.github.com>
2024-01-19 11:34:51 +0600

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IctFest
+{
+    public class Account
+    {
+        private static int currentBalance = 0;
+        private static List<int> unpaidIDs = new List<int>();
+
+        public static void AddToBalance(int amount)
+        {
+            currentBalance += amount;
+        }
+
+        public static void DisplayUnpaidIDs()
+        {
+            Console.WriteLine("Unpaid Participant and Team IDs:");
+            foreach (var id in unpaidIDs)
+            {
+                Console.WriteLine(id);
+            }
+        }
+
+        public static void UpdateUnpaidIDs(int id)
+        {
+            unpaidIDs.Add(id);
+        }
+    }
+}
