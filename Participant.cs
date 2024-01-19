15a7340a5430e2484baafc1c55c11d2b3c07d955
Iqra210042109 <128312944+Iqra210042109@users.noreply.github.com>
2024-01-19 11:38:39 +0600

    Add files via upload

---
+++ Participant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IctFest
+{
+    public class Participant : IParticipant, ITShirt
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Institution { get; set; }
+        public string TShirtSize { get; set; }
+
+        
+    }
+}
