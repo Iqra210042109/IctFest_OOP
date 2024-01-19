3018656b702be0ef19db4635f3e39bc064272463
Iqra210042109 <128312944+Iqra210042109@users.noreply.github.com>
2024-01-19 11:39:59 +0600

    Add files via upload

---
+++ Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IctFest
+{
+    public class Team: ITeam
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public List<IParticipant> Participants { get; set; }
+       
+    }
+}
